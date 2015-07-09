908e7a66237afa877d8fe9d23e92f326ebb758e2
KittyGarrison <katrina.garrison@detroitlabs.com>
2015-07-09 14:39:06 -0400

    displaying data from the controller

--- app.js
+++ app.js
@@ -3,5 +3,11 @@ var app = angular.module('flapperNews', []);
 app.controller('MainCtrl', [
 '$scope',
 function($scope){
-  $scope.test = 'Hello world!';
+  $scope.guests = [
+    {name_first: 'first name 1', isAttending: true},
+    {name_first: 'first name 2', isAttending: false},
+    {name_first: 'first name 3', isAttending: false},
+    {name_first: 'first name 4', isAttending: true},
+    {name_first: 'first name 5', isAttending: false}
+  ];
 }]);
