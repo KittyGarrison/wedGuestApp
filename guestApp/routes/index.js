38cd73c7a9cdd93c37c05f0b7406a19e90a7a484
KittyGarrison <katrina.garrison@detroitlabs.com>
2015-07-09 16:38:39 -0400

    routes test ok

--- guestApp/routes/index.js
+++ guestApp/routes/index.js
@@ -6,4 +6,26 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+var mongoose = require('mongoose');
+var Guest = mongoose.model('Guest');
+
+
+router.get('/guests', function(req, res, next) {
+  Guest.find(function(err, guest){
+    if(err){ return next(err); }
+
+    res.json(guest);
+  });
+});
+
+router.post('/guests/', function(req, res, next) {
+  var guest = new Guest(req.body);
+
+  guest.save(function(err, guest){
+    if(err){ return next(err); }
+
+    res.json(guest);
+  });
+});
+
 module.exports = router;
